925094661b69d649d7560575403b230926402281
KongkongNT15 <kongkong745837@gmail.com>
2025-05-01 21:36:01 +0900

    ビルドがより簡単に

---
+++ Build-Linux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# "build" ディレクトリが存在する場合は削除（中身ごと）
+if [ -d build ]; then
+    echo "Deleting existing build directory..."
+    rm -rf build
+fi
+
+# "build" ディレクトリを作成
+echo "Creating build directory..."
+mkdir build
+
+# コピー
+cp ./resources/build.sh ./build/
+cp ./resources/rebuild.sh ./build/
+
+# "build" ディレクトリに移動
+cd build
+
+# cmake の初期構成を実行
+echo "Running cmake .."
+cmake ..
+
+# Releaseビルドを実行
+echo "Building in Release mode..."
+cmake --build . --config Release
---
+++ Build-macOS.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# "build" ディレクトリが存在する場合は削除（中身ごと）
+if [ -d build ]; then
+    echo "Deleting existing build directory..."
+    rm -rf build
+fi
+
+# "build" ディレクトリを作成
+echo "Creating build directory..."
+mkdir build
+
+# コンパイラにgccを指定
+cmake -S . -B build -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+
+# コピー
+cp ./resources/build.sh ./build/
+cp ./resources/rebuild.sh ./build/
+
+# "build" ディレクトリに移動
+cd build
+
+# cmake の初期構成を実行
+echo "Running cmake .."
+cmake ..
+
+# Releaseビルドを実行
+echo "Building in Release mode..."
+cmake --build . --config Release
