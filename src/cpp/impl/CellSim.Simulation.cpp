a3b2f4217969d31f715bcc11310e34b8d6129631
KongkongNT15 <kongkong745837@gmail.com>
2025-06-15 23:17:22 +0900

    軽微な修正

--- src/cpp/impl/CellSim.Simulation.cpp
+++ src/cpp/impl/CellSim.Simulation.cpp
@@ -240,6 +240,9 @@ namespace CellSim
 
         m_writer.Save(*this, 0);
 
+        // 最後のステップでステップ数が２回表示されないようにする
+        bool isFinalStepPrint = false;
+
         for (uint64_t step = 1; step <= totalStep; ++step) {
 
             m_beforeAdvanceStep();
@@ -251,13 +254,15 @@ namespace CellSim
 
             if (::std::chrono::duration_cast<::std::chrono::milliseconds>(::std::chrono::system_clock::now() - currentClock).count() >= 250) {
                 ::printf("%llu/%llu\n", step, totalStep);
+                if (step == totalStep) isFinalStepPrint = true;
                 currentClock = ::std::chrono::system_clock::now();
             }
             
         }
 
         currentClock = ::std::chrono::system_clock::now();
-        ::printf("%llu/%llu\n", totalStep, totalStep);
+        
+        if (!isFinalStepPrint) ::printf("%llu/%llu\n", totalStep, totalStep);
 
         m_writer.SaveConfig(
             totalStep,
