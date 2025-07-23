052c1d291724c3f4cd1b7616cba3d68397e26738
KongkongNT15 <kongkong745837@gmail.com>
2025-07-24 07:25:24 +0900

    Molecular設定を追加

--- src/cpp/impl/CellSim.Settings.Config.Molecular.cpp
+++ src/cpp/impl/CellSim.Settings.Config.Molecular.cpp
@@ -1,1 +1,34 @@
-﻿
+﻿#include "CellSim.Settings.Config.Molecular.hpp"
+#include "CellSim.Messages.hpp"
+
+#include <stdexcept>
+#include <nlohmann/json.hpp>
+
+namespace CellSim::Settings
+{
+    void Config::Molecular::Load(::nlohmann::json const& config)
+    {
+        if (config.is_null()) [[unlikely]] throw ::std::runtime_error(Messages::Get("Settings.Config.Molecular.Load.Error.JsonError"));
+
+        ::std::string s1;
+        ::std::string s2;
+
+        try {
+            s1 = config.at("boundaryCondition").get<::std::string>();
+            s2 = config.at("initialDistribution").get<::std::string>();
+        }
+        catch (...) {
+            throw ::std::runtime_error(Messages::Get("Settings.Config.Molecular.Load.Error.JsonError"));
+        }
+
+        if (s1 == "Absorbing") s_boundaryCondition = ::CellSim::Molecular::BoundaryCondition::Absorbing;
+        else if (s1 == "Periodic") s_boundaryCondition = ::CellSim::Molecular::BoundaryCondition::Periodic;
+        else if (s1 == "Reflective") s_boundaryCondition = ::CellSim::Molecular::BoundaryCondition::Reflective;
+        else [[unlikely]] throw ::std::runtime_error(Messages::Get("Settings.Config.Molecular.Load.Error.boundaryCondition"));
+
+        if (s2 == "Centered") s_initialDistribution = ::CellSim::Molecular::InitialMoleculeDistribution::Centered;
+        else if (s2 == "Gaussian") s_initialDistribution = ::CellSim::Molecular::InitialMoleculeDistribution::Gaussian;
+        else if (s2 == "Uniform") s_initialDistribution = ::CellSim::Molecular::InitialMoleculeDistribution::Uniform;
+        else [[unlikely]] throw ::std::runtime_error(Messages::Get("Settings.Config.Molecular.Load.Error.initialDistribution"));
+    }
+}
