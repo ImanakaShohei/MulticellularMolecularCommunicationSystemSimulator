242bc84defd2f4bb56250fc0c1890360ed84816f
KongkongNT15 <kongkong745837@gmail.com>
2025-08-08 03:46:54 +0900

    GUIのプロジェクトを追加

---
+++ src/ui/CellSimulator/CellSimulator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CellSimulator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/ui/CellSimulator/CellSimulator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CellSimulator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
