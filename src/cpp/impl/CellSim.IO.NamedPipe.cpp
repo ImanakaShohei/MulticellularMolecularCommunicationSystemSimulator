c5d0c3b4ba84f83824a96194b12add7af026c350
KongkongNT15 <kongkong745837@gmail.com>
2025-08-21 14:05:17 +0900

    NamedPipeクラスを追加

---
+++ src/cpp/impl/CellSim.IO.NamedPipe.cpp
@@ -0,0 +1,7 @@
+﻿#include "CellSim.IO.NamedPipe.hpp"
+
+namespace CellSim::IO
+{
+    NamedPipe NamedPipe::s_in;
+    NamedPipe NamedPipe::s_out;
+}
---
+++ src/cpp/impl/CellSim.IO.NamedPipe.hpp
@@ -0,0 +1,31 @@
+﻿#ifndef CELLSIM_IO_NAMEDPIPE_HPP
+#define CELLSIM_IO_NAMEDPIPE_HPP
+
+#include "base.hpp"
+
+#if CELLSIM_ENV_WINDOWS
+    #include <Windows.h>
+#elif CELLSIM_ENV_UNIX
+
+#endif
+
+namespace CellSim::IO
+{
+    /// @brief 名前付きパイプ
+    class NamedPipe final {
+        friend Cli::CliMain;
+        private:
+        static NamedPipe s_in;
+        static NamedPipe s_out;
+
+#if CELLSIM_ENV_WINDOWS
+        ::HANDLE m_pipeHandle;
+#elif CELLSIM_ENV_UNIX
+
+#endif
+
+        public:
+    };
+}
+
+#endif //!CELLSIM_IO_NAMEDPIPE_HPP
