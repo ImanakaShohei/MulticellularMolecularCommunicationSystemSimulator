41ebf8f1f53ee07bbe30c372301cbfa9b7f2aee0
KongkongNT15 <kongkong745837@gmail.com>
2025-09-05 17:36:36 +0900

    軽微な修正

--- src/cpp/impl/CellSim.Cli.ParamSweepOption.cpp
+++ src/cpp/impl/CellSim.Cli.ParamSweepOption.cpp
@@ -61,6 +61,7 @@ namespace CellSim::Cli
                 args.Options->at(CliOptionType::Csv)->IsEnabled(),
                 args.Options->at(CliOptionType::Image)->IsEnabled(),
                 args.Options->at(CliOptionType::Video)->IsEnabled(),
+                args.Options->at(CliOptionType::CleanOutput)->IsEnabled(),
                 ::std::move(path)
             };
 
