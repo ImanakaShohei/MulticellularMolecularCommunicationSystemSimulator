f64435ac567092e93be45f98a75d5252511fc31a
KongkongNT15 <kongkong745837@gmail.com>
2025-09-16 20:24:12 +0900

    2.1.0

--- src/cpp/impl/CellSim.Version.hpp
+++ src/cpp/impl/CellSim.Version.hpp
@@ -8,7 +8,7 @@ namespace CellSim
     class Version final {
         private:
 
-        static constexpr const char s_extension[] = "Beta 1";
+        static constexpr const char s_extension[] = "";
 
         public:
 
